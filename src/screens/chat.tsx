f63b31540c8a3b9e9084e65edc5c8c184671c9b5
Stanley Filipe <stanley92filipe@gmail.com>
2022-11-01 18:32:00 -0300

    feat: chat screen

--- src/screens/chat.tsx
+++ src/screens/chat.tsx
@@ -1,8 +1,31 @@
+import { FormEvent, useRef } from "react"
+
 function ChatScreen() {
+
+  const new_message_input_ref = useRef<HTMLInputElement>(null)
+
+  function handleSendANewMessage(event: FormEvent) {
+    event.preventDefault()
+
+    if (!new_message_input_ref.current) return
+
+    if (!new_message_input_ref.current.value) return
+
+    console.log(new_message_input_ref.current.value)
+
+    new_message_input_ref.current.value = ''
+  }
+
   return (
-    <h1>
-      chat screen
-    </h1>
+    <main className="h-screen w-screen bg-neutral-900 flex flex-col justify-center items-center p-4 md:p-16">
+      <div className="flex-1">
+
+      </div>
+      <form onSubmit={handleSendANewMessage} className='flex w-full gap-2'>
+        <input ref={new_message_input_ref} placeholder="Escreva uma mensagem" className="w-full bg-neutral-800 h-10 rounded placeholder-gray-400 text-slate-100 px-4" />
+        <button type="submit" className="w-1/6 bg-neutral-600 h-10 rounded text-slate-100 px-4 hover:bg-neutral-500 font-bold">Enviar</button>
+      </form>
+    </main>
   )
 }
 
