6f45aba895da1b9e8df1de291aee1ec0dd2844bd
Stanley Filipe <stanley92filipe@gmail.com>
2022-11-12 13:38:18 -0300

    feat: create only lower case rooms

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -28,14 +28,14 @@ function HomeScreen() {
 
     setUsername(username_input.current.value)
 
-    return navigate(`/room/${room_id_value}`)
+    return navigate(`/room/${room_id_value.toLowerCase()}`)
   }
   
   return (
     <main className="h-screen w-screen bg-neutral-900 flex flex-col justify-center items-center">
       <form onSubmit={handleSignIn} className="w-5/6 md:w-2/5 lg:w-3/12 flex flex-col items-center gap-4">
         <HexColorPicker color={color} onChange={setColor} className="mb-4" />
-        <input ref={room_id_input} placeholder="Código da sala" className="w-full bg-neutral-800 h-10 rounded placeholder-gray-400 text-slate-100 px-4" />
+        <input ref={room_id_input} placeholder="Código da sala" className="w-full bg-neutral-800 h-10 rounded placeholder-gray-400 text-slate-100 px-4 lowercase" />
         <input defaultValue={username} ref={username_input} placeholder="Username" className="w-full bg-neutral-800 h-10 rounded placeholder-gray-400 text-slate-100 px-4" />
         <button type="submit" className="w-full bg-neutral-600 h-10 rounded text-slate-100 px-4 hover:bg-neutral-500 font-bold">Entrar na sala</button>
       </form>
